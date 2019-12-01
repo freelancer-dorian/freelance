86b92af3ee65adf43b656755841c3e8f62a30d92
wanghui <wanghui@Mac.local>
2019-12-01 14:19:46 +0800

    commit for chapter 5

--- SQL_Notes/basic_exercises.sql
+++ SQL_Notes/basic_exercises.sql
@@ -16,3 +16,18 @@ ALTER TABLE Addressbook ADD COLUMN postal_code CHAR(8) NOT NULL;
 DROP TABLE Addressbook;
 
 
+-- 1.SELECT子句→2.FROM子句→3.WHERE子句→4.GROUP BY子句→ 5. HAVING 子句 → 6. ORDER BY 子句
+
+
+-- exe5.1
+CREATE VIEW View_5_1 (product_name, sale_price, regist_date) AS SELECT product_name, sale_price, regist_date FROM Product
+WHERE sale_price >= 1000 AND regist_date = '2009-09-20';
+
+-- exe5.3
+SELECT product_id, product_name, product_type, sale_price, (SELECT AVG(sale_price) FROM Product) AS sale_price_all FROM Product;
+
+-- exe5.4
+CREATE VIEW AvgPriceByType (product_id,product_name, product_type, sale_price, avg_sale_price) AS 
+SELECT product_id,product_name, product_type, sale_price, (SELECT AVG(sale_price) FROM Product AS P2 WHERE P1.product_type = P2.product_type GROUP BY product_type) as avg_sale_price FROM Product AS P1;
+
+
